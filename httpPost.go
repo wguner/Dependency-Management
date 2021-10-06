f8cc40d7e93a7948e63f479be5001c4c40671247
Setenay <setenay.guner@wsu.edu>
2021-10-05 17:17:13 -0700

    adding comments

--- httpPost.go
+++ httpPost.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-
+	// A map is serialized into JSON string with json.Marshal. 
 	values := map[string]string{"name": "John Doe", "occupation": "gardener"}
 	json_data, err := json.Marshal(values)
 
@@ -22,7 +22,7 @@ func main() {
 	// PostForm issues a POST to the specified URL, with data's keys and values URL-encoded as the request body. 
 	resp, err := http.Post("https://httpbin.org/post", "application/json",
 		bytes.NewBuffer(json_data))
-
+	// When we post the data, we set the content type to application/json. 
 	if err != nil {
 		log.Fatal(err)
 	}
