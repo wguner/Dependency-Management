61e4383f9c7c245f1068131bdbee23e57862bc23
eaguiler <elisha.aguilera@wsu.edu>
2021-10-07 11:41:06 -0700

    Changed client script, configs.

--- Client/package-bird-client-env/Src/main.py
+++ Client/package-bird-client-env/Src/main.py
@@ -1,10 +1,23 @@
 import argparse
+import requests
 
 # Server address
-server_adr = 'localhost'
+server_adr = 'http://localhost:8080'
 
 # Contact the server, with custom address
 parser = argparse.ArgumentParser(description='Contacts the token server.')
-parser.add_argument('address', metavar=server_adr, type=str, nargs='?', help='The address of the server to conact')
+parser.add_argument('address', metavar='server_adr', type=str, nargs='?', help='The address of the server to conact')
 
-# More code below
+ars = parser.parse_args()
+
+# Connect to server endpoint, await response
+request = requests.get(server_adr)
+
+# Get status
+status = request.status_code
+
+# Parse to JSON
+response = request.json()
+
+# Print status and payload
+print(f'Status Code: {status}\nPayload:\n{response}')
