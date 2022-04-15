51ce7361ac5bbb18dd63f4232a30cb8f210ed346
eaguile1 <elisha.aguilera@wsu.edu>
2022-04-14 19:55:03 -0700

    Added VS C# WinForm Project, populated with few buttons

---
+++ Graphical Interface/Packagebird Graphical Client/Form1.cs
@@ -0,0 +1,15 @@
+namespace Packagebird_Graphical_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
